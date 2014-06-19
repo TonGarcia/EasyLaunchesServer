ca882f1dc01c455a0481cf1872f415073d28121a
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-18 21:14:07 -0300

    ZeroPaper & ContaAzul base structure Lib for connection added

---
+++ lib/remote_api/zeropaper.rb
@@ -0,0 +1,10 @@
+module REMOTE_API
+
+  # Class for specific implementation of ZeroPaper API connection
+  class ZeroPaper
+
+
+
+  end
+
+end
---
+++ spec/lib/zeropaper_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require 'its'
+include REMOTE_API
+
+describe REMOTE_API::ZeroPaper do
+
+
+
+end
