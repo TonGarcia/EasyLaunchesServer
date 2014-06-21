29034a21eff926607c4e1e406572506e1dea0a38
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-21 19:43:55 -0300

    ROUTES addeds: - /transactions (our MobApp POSTs); - /clients (business parnters GTEs, which we do not implement).

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root to: 'landing_page#index'
 
+  namespace :api do
+    # API routes for our MobileApps POST
+    scope '/transactions' do
+      post '/create' => 'transactions#create'
+    end
+
+    # API routes for Clients GET (business partners whose we do not implment POSTs)
+    scope '/clients' do
+
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
