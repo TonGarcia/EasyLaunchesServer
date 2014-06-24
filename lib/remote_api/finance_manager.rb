8a4ee0f10fb29b27ad781b0fe5159f1693aebb7c
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-24 17:01:28 -0300

    base FinanceManager abstract class

--- lib/remote_api/finance_manager.rb
+++ lib/remote_api/finance_manager.rb
@@ -3,7 +3,7 @@ module REMOTE_API
   class FinanceManager
     # Abstract methods
     private_class_method :do_login
-    private_class_method :send_launches
     private_class_method :connected?
+    private_class_method :send_launches
   end
 end
