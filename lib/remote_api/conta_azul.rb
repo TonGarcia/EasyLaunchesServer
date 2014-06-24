42d5f5ca3ce418c8dc642887c3475832e764371c
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-24 17:01:51 -0300

    abstract methods added

--- lib/remote_api/conta_azul.rb
+++ lib/remote_api/conta_azul.rb
@@ -1,10 +1,8 @@
 module REMOTE_API
-
   # Class for specific implementation of ContaAzul API connection
-  class ContaAzul
-
-
-
+  class ContaAzul < REMOTE_API::FinanceManager
+    public_class_method :do_login
+    public_class_method :connected?
+    public_class_method :send_launches
   end
-
 end
--- lib/remote_api/zero_paper.rb
+++ lib/remote_api/zero_paper.rb
@@ -1,17 +1,25 @@
 module REMOTE_API
 
   # Class for specific implementation of ZeroPaper API connection
-  class ZeroPaper
+  class ZeroPaper < REMOTE_API::FinanceManager
+    public_class_method :do_login
+    public_class_method :connected?
+    public_class_method :send_launches
+
     # On instantiate ZP API auth
     def initialize
       auth_user
     end
 
     # Authenticate the user
-    def auth_user
+    def do_login
       uri = URI.parse('https://zeropaper.com.br/api/v1/auth/create')
       res = Net::HTTP.post_form(uri, {email: 'ilton_unb@hotmail.com', password: 'u5ga6f123'})
       res.body
+    end
+
+    def send_launches
+
     end
 
     # Return if the user was correctly created
