f94fa13c224fb0cf67467ba367cd17a37de7a3c2
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-24 22:25:22 -0300

    class_method changed to intstance_method

--- lib/remote_api/conta_azul.rb
+++ lib/remote_api/conta_azul.rb
@@ -1,9 +1,9 @@
 module REMOTE_API
   # Class for specific implementation of ContaAzul API connection
   class ContaAzul < REMOTE_API::FinanceManager
-    public_class_method :do_login
-    public_class_method :connected?
-    public_class_method :send_launches
+    public_instance_method :do_login
+    public_instance_method :connected?
+    public_instance_method :send_launches
 
     # Try to login on the remote
     def do_login email, password
--- lib/remote_api/finance_manager.rb
+++ lib/remote_api/finance_manager.rb
@@ -2,8 +2,20 @@ module REMOTE_API
   # Abstract class to specify the REMOTE_APIs behaviours
   class FinanceManager
     # Abstract methods
-    private_class_method :do_login # Try to login on the remote
-    private_class_method :connected? # Return true if success on remote
-    private_class_method :send_launches # If connected send the values to the remote as the current_user
+    private_instance_methods :do_login
+    private_instance_methods :connected?
+    private_instance_methods :send_launches
+
+    # Try to login on the remote
+    def do_login
+    end
+
+    # Return true if success on remote
+    def connected?
+    end
+
+    # If connected send the values to the remote as the current_user
+    def send_launches
+    end
   end
 end
