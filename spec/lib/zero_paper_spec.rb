db1bd8e5cd724f91a61475f05d85be83c8b16203
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-24 22:26:20 -0300

    Class Diagram UPDATED & ZP Spec (methods renamed)

--- spec/lib/zero_paper_spec.rb
+++ spec/lib/zero_paper_spec.rb
@@ -6,6 +6,6 @@ describe REMOTE_API::ZeroPaper do
   subject(:zp_client) { REMOTE_API::ZeroPaper.new }
   let(:zp_client) { REMOTE_API::ZeroPaper.new }
 
-  it { should be_conected }
-  its(:auth_user) { should be_true }
+  it { should be_connected }
+  its(:do_login, 'ilton_unb@hotmail.com', 'u5ga6f123') { should be_true }
 end
