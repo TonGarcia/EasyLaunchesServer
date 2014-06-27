1707e811a75781311b1286e9ca83d502e810f9bf
Ilton Garcia <ilton_unb@hotmail.com>
2014-06-26 22:08:37 -0300

    REFACTORED: not Transaction (reserved method) -> FinancialTransaction & NOT NULL updated

--- db/migrate/20140618144652_create_financial_transactions.rb
+++ db/migrate/20140618144652_create_financial_transactions.rb
@@ -1,10 +1,10 @@
-class CreateTransactions < ActiveRecord::Migration
+class CreateFinancialTransactions < ActiveRecord::Migration
   def change
-    create_table :transactions do |t|
+    create_table :financial_transactions do |t|
       t.float :value, null: false
-      t.string :date, null: true
+      t.string :date, null: false
       t.string :category, null: true
-      t.string :involved_person, null: true
+      t.string :involved_person, null: false
       t.belongs_to :transaction_type, index: true
       t.belongs_to :finance_manager_session, index: true
 
